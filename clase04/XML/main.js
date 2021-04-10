99716c9c56545dde123b11366decf191f6d0d28a
unknown <jtorriccelli1986@gmail.com>
2021-04-10 10:29:04 -0500

    update

---
+++ clase04/XML/main.js
@@ -0,0 +1,111 @@
+//let url_categoria="http://sminnova.com/restapitrujillo/sitiosturisticos/categoria/ID"
+//let url_listado="http://sminnova.com/restapitrujillo/sitiosturisticos"
+
+let url1="informacion.json";
+let url2="informacion2.json";
+
+
+let opcion1=document.getElementById("opcion1")
+let opcion2=document.getElementById("opcion2")
+let cantidad_elementos;
+let listado_contenido=document.getElementById("listado-contenido");
+
+opcion1.addEventListener("click",()=>{
+
+    let xhr=new XMLHttpRequest()
+    xhr.open("GET",url1)
+    xhr.send();
+        xhr.onreadystatechange=function(){
+            if(xhr.readyState==4){
+                //console.log(xhr.responseText);
+                let resultado=JSON.parse(xhr.responseText);
+                console.log(resultado);
+                cantidad_elementos=resultado.length;
+                let content="";
+               /* for(let item of resultado){
+                    content=content+` <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${item.titulo}</h5>
+                      <p class="card-text">${item.contenido}</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>`;
+                }*/
+                for(let i=0;i<cantidad_elementos;i++){
+                    /*resultado[i].titulo
+                    resultado[i].contenido*/
+
+                    content=content+` <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">${  resultado[i].titulo}</h5>
+                      <p class="card-text">${resultado[i].cotenido}</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>`;
+
+
+                }
+
+
+
+                listado_contenido.innerHTML=content;
+
+            
+
+            }
+        }
+
+
+})
+
+opcion2.addEventListener("click",()=>{
+
+    let xhr=new XMLHttpRequest()
+    xhr.open("GET",url2)
+    xhr.send();
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState==4){
+            //console.log(xhr.responseText);
+            let resultado=JSON.parse(xhr.responseText);
+            console.log(resultado);
+            cantidad_elementos=resultado.length;
+            let content="";
+           /* for(let item of resultado){
+                content=content+` <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${item.titulo}</h5>
+                  <p class="card-text">${item.contenido}</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>`;
+            }*/
+            for(let i=0;i<cantidad_elementos;i++){
+                /*resultado[i].titulo
+                resultado[i].contenido*/
+
+                content=content+` <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${  resultado[i].titulo}</h5>
+                  <p class="card-text">${resultado[i].cotenido}</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>`;
+
+
+            }
+
+
+
+            listado_contenido.innerHTML=content;
+
+        
+
+        }
+    }
+    console.log(cantidad);
+})
+
